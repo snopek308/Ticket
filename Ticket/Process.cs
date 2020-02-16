0e639f1957ef22bfdd79c4a0bf2fce1482c15347
asnopek <asnopek486@gmail.com>
2020-02-15 22:49:33 -0600

    Add project files.

---
+++ Ticket/Process.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ticket
+{
+    class Process
+    {
+        String fName = "";
+        String lName = "";
+        String gender = "";
+        String genderUpper = "";
+        int age = 0;
+        String result = "";
+
+        //retrieve data
+        public void Retrieve()
+        {
+            Console.WriteLine("Please enter your first name: ");
+            fName = Console.ReadLine();
+            Console.WriteLine("Please enter your age: ");
+            age = Convert.ToInt32(Console.ReadLine());
+        }
+
+        //process data
+        public void processData()
+        {
+            if (age < 18)
+            {
+                result = fName + " X";
+            }
+            else
+            {
+                Console.WriteLine("Please enter your last name: ");
+                lName = Console.ReadLine();
+                Console.WriteLine("Please enter your gender: F or M");
+                gender = Console.ReadLine();
+                genderUpper = gender.ToUpper();
+
+                if(genderUpper == "F")
+                {
+                    result = "Ms " + lName + " is allowed into the program";
+                }
+                else
+                {
+                    result = "Mr " + lName + " is allowed into the program";
+                }
+                
+            }
+        }
+        
+        public String returnResult()
+        {
+            return result;
+        }
+
+        //returnResult
+    }
+}
---
+++ Ticket/Receptionist.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ticket
+{
+    class Receptionist
+    {
+        //ticketPrint
+        int numApplicants = 0;
+        String finalResult = "";
+
+        public void ticketPrint()
+        {
+            Console.WriteLine("Enter the number of applicants: ");
+            numApplicants = Convert.ToInt32(Console.ReadLine());
+
+            for(int i = 0; i < numApplicants; i++)
+            {
+                Process temp = new Process();
+                temp.Retrieve();
+                temp.processData();
+                finalResult = finalResult + "\n" + temp.returnResult();
+            }
+            Console.WriteLine(finalResult);
+        }
+
+    }
+}
